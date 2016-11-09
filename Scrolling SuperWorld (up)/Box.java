059aa93e75f8a48637c2dec9c97914da6ee62911
andrikonis <andrikonis.andrius@gmail.com>
2016-11-09 12:42:22 +0100

    Box

--- Scrolling SuperWorld (up)/Box.java
+++ Scrolling SuperWorld (up)/Box.java
@@ -10,6 +10,24 @@ public class Box extends Actor
      */
     public Box()
     {
-        getImage().scale(50, 50);
+        
+    }
+    public Box(int a,int size){
+        GreenfootImage box = new GreenfootImage("Box(50x50).png");
+        if(a==0){
+            GreenfootImage image = new GreenfootImage(size, box.getHeight());
+            int w=box.getWidth();
+            for(int offset=0; offset<size; offset+=w) image.drawImage(box, offset, 0);
+            setImage(image);
+        }
+        else if(a==1){
+            GreenfootImage image = new GreenfootImage(box.getWidth(),size);
+            int w=box.getHeight();
+            for(int offset=0; offset<size; offset+=w) image.drawImage(box, 0, offset);
+            setImage(image);
+        }
+        else{
+            System.out.println("Error");
+        }
     }
 }
