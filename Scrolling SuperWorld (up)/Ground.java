5a1a4a956da3d04a04ddc88a28f17d29c0251d11
andrikonis <andrikonis.andrius@gmail.com>
2016-11-09 11:53:41 +0100

    Little edits for world creation

--- Scrolling SuperWorld (up)/Ground.java
+++ Scrolling SuperWorld (up)/Ground.java
@@ -8,18 +8,12 @@ public class Ground extends Actor
     /**
      * Creates a long platform for the main actor.
      */
-    public Ground()
+    public Ground(int size)
     {
         GreenfootImage road = new GreenfootImage("Ground.png");
-        GreenfootImage image = new GreenfootImage(1040, road.getHeight());
-        image.fill();
+        GreenfootImage image = new GreenfootImage(size, road.getHeight());
         int w=road.getWidth();
-        
-        
-        for(int offset=0; offset<1040; offset+=w) 
-        {
-            image.drawImage(road, offset, 0);
-        }
+        for(int offset=0; offset<size; offset+=w) image.drawImage(road, offset, 0);
         setImage(image);
     }
 }
--- Scrolling SuperWorld (up)/MyWorld.java
+++ Scrolling SuperWorld (up)/MyWorld.java
@@ -11,22 +11,20 @@ public class MyWorld extends SWorld
      */
     public MyWorld()
     {    
-        super(800, 600, 1, 6000); // scroll world constructor call; last parameter is scroll width
+        super(700, 500, 1, 6000); // scroll world constructor call; last parameter is scroll width
         // in the following statement, the main actor is placed in the center of the window
-        setMainActor(new Wombat(), 250, 300); // the int parameters are centered window x and y ranges
+        setMainActor(new Wombat(),100, getHeight()-75); // the int parameters are centered window x and y ranges
         // to start the main actor elsewhere
-        mainActor.setLocation(100, 342);
-        GreenfootImage bg = new GreenfootImage("Level1.png");
+        mainActor.setLocation(100, getHeight()-75);
+        GreenfootImage bg = new GreenfootImage("Mario LevelBG.png");
         setScrollingBackground(bg); // set the scolling background image
         // add other scrollable objects normally
-        addObject(new Ground(), 600, 600);
-
-        addObject(new Ground(), 2000, 600);
-        addObject(new Box(), 20, 345);
+        addObject(new Ground(5000), 200,getHeight()-25);
+        addObject(new Box(), 20,getHeight()-75);
         // use of the following also adds scrollable objects
-        addObject(new Box(), 380, 345, true); // the boolean determines scrollable state
+        addObject(new Box(), 380, getHeight()-150, true); // the boolean determines scrollable state
         // use the following for non-scrollable objects
-        addObject(new Score(), 40, 390, false);
+        addObject(new Score(), 40,getHeight()-50, false);
         prepare();
     }
 
@@ -37,6 +35,14 @@ public class MyWorld extends SWorld
     private void prepare()
     {
         Box box = new Box();
-        addObject(box,390,452);
+        addObject(box,1108,34);
+        Box box2 = new Box();
+        addObject(box2,1115,133);
+        Box box3 = new Box();
+        addObject(box3,1119,237);
+        Box box4 = new Box();
+        addObject(box4,1127,353);
+        Box box5 = new Box();
+        addObject(box5,1126,417);
     }
 }
